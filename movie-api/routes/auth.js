6ce0dd0d7d2e23b75e8451ec69af0846e604f38c
Santosnr6 <77623157+Santosnr6@users.noreply.github.com>
2024-05-24 08:23:28 +0200

    Update auth.js

--- movie-api/routes/auth.js
+++ movie-api/routes/auth.js
@@ -1,46 +1,57 @@
-import { Router } from "express";
-import { users } from "../config/data.js";
-import userSchema from "../models/userModel.js";
+import { Router } from 'express';
+import { users }  from '../config/data.js';
+import userSchema from '../models/userSchema.js';
 
 const router = Router();
 
 router.post('/login', (req, res) => {
-    const {error} = userSchema.validate(req.body);
-    console.log(error);
-
-    if(error) {
-        return res.status(400).json({message: error.details[0].message});
+    const { error } = userSchema.validate(req.body);
+    if(error){
+        const response = {
+            success: false,
+            message: error.details[0].message,
+            status: 400
+        }
+        return res.status(400).json(response);
     }
 
     const { username, password } = req.body;
     const user = users.find(user => user.username === username && user.password === password);
-    if (user) {
+    if(user) {
         global.currentUser = user;
-        res.json({ message: 'Login successful' });
+        res.json({ message : 'Login successful', user : user });
     } else {
-        res.status(401).json({ message: 'Unauthorized' });
+        res.status(401).json({ message : 'Invalid credentials' });
     }
 });
 
 router.post('/logout', (req, res) => {
     global.currentUser = null;
-    res.json({ message: 'Logout successful' });
+    res.json({ message : 'Logged out successfully' });
 });
 
 router.post('/register', (req, res) => {
-    const {error} = userSchema.validate(req.body);
-
-    if(error) {
-        return res.status(400).json({message: error.details[0].message});
+    const { error } = userSchema.validate(req.body);
+    if(error){
+        const response = {
+            success: false,
+            message: error.details[0].message,
+            status: 400
+        }
+        return res.status(400).json(response);
     }
-    
+
     const { username, password } = req.body;
     const user = users.find(user => user.username === username);
-    if (user) {
-        res.status(400).json({ message: 'User already exists' });
+    if(user) {
+        res.status(400).json({ message : 'User already exists' });
     } else {
-        users.push({ id: users.length + 1, username, password });
-        res.json({ message: 'User created' });
+        users.push({
+            id : users[users.length - 1].id + 1,
+            username : username,
+            password : password
+        });
+        res.json({ message : 'User registered successfully' });
     }
 });
 
